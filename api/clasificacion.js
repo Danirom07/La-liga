a4003f524e8e52381c275f9d4cb6417d88c7b768
Diego Alor. <diealoher2@alu.edu.gva.es>
2026-03-09 17:13:33 +0100

    crear api clasificacion

--- api/clasificacion.js
+++ api/clasificacion.js
@@ -1,1 +1,15 @@
+export default async function handler(req, res) {
 
+  const response = await fetch(
+    "https://api.football-data.org/v4/competitions/PD/standings",
+    {
+      headers: {
+        "X-Auth-Token": "TU_API_KEY"
+      }
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
